150cb3f5724eb4af6ead0591c3e5a6763c744c53
Asus <sofiyakamalova1805@gmail.com>
2023-02-09 10:49:55 +0600

    commit

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,5 +3,8 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
+        System.out.print("java\n");
+        System.out.print("is\n");
+        System.out.println("smth\n");
     }
 }
